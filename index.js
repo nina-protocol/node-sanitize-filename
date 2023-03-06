17c02764158519ab8a6652bec8a0297daf4f50b9
facing-n <mike@ninaprotocol.com>
2023-03-06 14:36:09 -0500

    add + and ' '

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ var controlRe = /[\x00-\x1f\x80-\x9f]/g;
 var reservedRe = /^\.+$/;
 var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
 var windowsTrailingRe = /[\. ]+$/;
+var plusSign = /\+/g;
+var blankSpace = / /g;
 
 function sanitize(input, replacement) {
   if (typeof input !== 'string') {
@@ -45,7 +47,9 @@ function sanitize(input, replacement) {
     .replace(controlRe, replacement)
     .replace(reservedRe, replacement)
     .replace(windowsReservedRe, replacement)
-    .replace(windowsTrailingRe, replacement);
+    .replace(windowsTrailingRe, replacement)
+    .replace(plusSign, replacement)
+    .replace(blankSpace, replacement);
   return truncate(sanitized, 255);
 }
 
